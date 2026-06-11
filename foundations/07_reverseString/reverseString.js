7c70fc088a837c294fbcb2b000334e5c0320c7fb
Yusuf <236015542+yusuf1425-rgb@users.noreply.github.com>
2026-06-11 15:38:13 +0530

    Complete javascript exercise 07_reverseString

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,6 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(text) {
+    return text.split("").reverse().join("");
 };
+
+console.log(reverseString("hello there"));
 
 // Do not edit below this line
 module.exports = reverseString;
