50c2cc648e103261545a895dd28779d5b2c1357c
Yusuf <236015542+yusuf1425-rgb@users.noreply.github.com>
2026-06-11 16:42:04 +0530

    Complete javascript exercise 10_leapYears

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,6 +1,10 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if (year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0)){
+        return true;
+    } else {
+        return false;
+    }
 };
-
+console.log(leapYears(1800));
 // Do not edit below this line
 module.exports = leapYears;
