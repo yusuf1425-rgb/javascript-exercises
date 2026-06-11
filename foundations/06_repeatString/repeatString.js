4a725066dd2b4b87888cb8d6f993881db8ea0b8f
Yusuf <236015542+yusuf1425-rgb@users.noreply.github.com>
2026-06-11 15:19:38 +0530

    Complete javascript exercise 06_repeatString

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(text, num) {
+    let repeatText = "";
+    if(num < 0){
+        return "ERROR";
+    }
+    for(let i = 1; i <= num; i++){
+        repeatText += text;
+    }
+    return repeatText;
 };
 
 // Do not edit below this line
