027b5ec9deaed44f0a255fdef325b0b2b2bad276
Yusuf <236015542+yusuf1425-rgb@users.noreply.github.com>
2026-06-11 16:10:24 +0530

    Complete javascript exercise 08_removeFromArray

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
+/**
+ * @param {Array} arr 
+ * @param {string} text 
+ */
+const removeFromArray = function(arr, element) {
+    return arr.filter((e) => e !== element);
 };
+
+console.log(removeFromArray([1, 2, 3, 4, 3], 3));
 
 // Do not edit below this line
 module.exports = removeFromArray;
