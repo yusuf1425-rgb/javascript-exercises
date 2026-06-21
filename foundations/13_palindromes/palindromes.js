add5d96b5791fe17dc1b572eb1df81c4d0ee2a54
Yusuf <236015542+yusuf1425-rgb@users.noreply.github.com>
2026-06-21 11:01:10 +0530

    Complete javascript exercise 13_palindromes

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,6 +1,10 @@
-const palindromes = function () {
-
+const palindromes = (text) => {
+    const cleanText = text.toLowerCase().replace(/[^A-Za-z0-9]/g, "");
+    return cleanText === cleanText.split("").reverse().join("");
 };
+
+console.log(palindromes('r3ace3car'));
+console.log(palindromes('tacos'));
 
 // Do not edit below this line
 module.exports = palindromes;
