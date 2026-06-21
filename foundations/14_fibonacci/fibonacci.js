74b18b14989a48a98cb0501e1b66868f54460dd8
Yusuf <236015542+yusuf1425-rgb@users.noreply.github.com>
2026-06-21 11:24:16 +0530

    Complete javascript exercise 14_fibonacci

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if(num < 0){
+        return "OOPS";
+    }
+    let a = 0, b = 1, nextTerm = 0;
 
+    for(let i = 0; i < num; i++){
+        nextTerm = a + b;
+        a = b;
+        b = nextTerm;
+    }
+    return a;
 };
 
 // Do not edit below this line
