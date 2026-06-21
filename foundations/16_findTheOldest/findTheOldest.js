9082c26bdab0101cc61d8fd242df69e137d8f830
Yusuf <236015542+yusuf1425-rgb@users.noreply.github.com>
2026-06-21 12:28:35 +0530

    Complete javascript exercise 16_findTheOldest

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,6 +1,34 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    people.sort((personA, personB) => getAge(personA) - getAge(personB));
+    return people[people.length - 1];
 };
 
+const getAge = function(person){
+    if(!person.yearOfDeath){
+        const currentYear = new Date().getFullYear();
+        return currentYear - person.yearOfBirth;
+    }
+    return person.yearOfDeath - person.yearOfBirth;
+};
+
+const people = [
+      {
+        name: "Carly",
+        yearOfBirth: 1942,
+      },
+      {
+        name: "Ray",
+        yearOfBirth: 1942,
+        yearOfDeath: 2011,
+      },
+      {
+        name: "Jane",
+        yearOfBirth: 1912,
+        yearOfDeath: 2941,
+      },
+    ]
+
+console.log(findTheOldest(people));
+    
 // Do not edit below this line
 module.exports = findTheOldest;
