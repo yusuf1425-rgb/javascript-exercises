aee7fe1615535a5666615e79f8939c50a3ec4853
Yusuf <236015542+yusuf1425-rgb@users.noreply.github.com>
2026-06-21 11:32:50 +0530

    Complete javascript exercise 15_getTheTitles

--- foundations/15_getTheTitles/getTheTitles.js
+++ foundations/15_getTheTitles/getTheTitles.js
@@ -1,6 +1,22 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    return books.reduce((bookList, bookObj) => {
+        bookList.push(bookObj.title);
+        return bookList;
+    }, []);
 };
+
+const books = [
+  {
+    title: 'Book',
+    author: 'Name'
+  },
+  {
+    title: 'Book2',
+    author: 'Name2'
+  }
+]
+
+console.log(getTheTitles(books));
 
 // Do not edit below this line
 module.exports = getTheTitles;
